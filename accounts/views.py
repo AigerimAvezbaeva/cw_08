df9e457aa7e324e84f890c4ea465b4a35d4238a8
Aigerim Avezbaeva <av21m1999@gmail.com>
2023-03-25 12:24:14 +0600

    добавила вьюшки, формы, пути приложения для аккаунтов

---
+++ accounts/views.py
@@ -0,0 +1,85 @@
+from django.contrib.auth import authenticate, login, logout, get_user_model, update_session_auth_hash
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.handlers.wsgi import WSGIRequest
+from django.core.paginator import Paginator
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.views.generic import TemplateView, CreateView, DetailView, UpdateView
+
+from accounts.forms import LoginForm, CustomUserCreationForm, UserChangeForm
+
+
+class LoginView(TemplateView):
+    template_name = 'login.html'
+    form = LoginForm
+
+    def get(self, request: WSGIRequest, *args, **kwargs):
+        next = request.GET.get('next')
+        form_data = {} if not next else {'next': next}
+        form = self.form(form_data)
+        context = {'form': form}
+        return self.render_to_response(context)
+
+    def post(self, request, *args, **kwargs):
+        form = self.form(request.POST)
+        if not form.is_valid():
+            return redirect('login')
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        next = form.cleaned_data.get('next')
+        user = authenticate(request, username=username, password=password)
+        if not user:
+            return redirect('login')
+        login(request, user)
+        if next:
+            return redirect(next)
+        return redirect('products_index')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('products_index')
+
+
+class RegisterView(CreateView):
+    template_name = 'register.html'
+    form_class = CustomUserCreationForm
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(self.get_success_url())
+        context = {}
+        context['form'] = form
+        return self.render_to_response(context)
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if not next_url:
+            next_url = self.request.POST.get('next')
+        if not next_url:
+            next_url = reverse('show_products')
+        return next_url
+
+
+class UserView(LoginRequiredMixin, DetailView):
+    model = get_user_model()
+    template_name = 'user_detail.html'
+    context_object_name = 'user_obj'
+
+
+class UserChangeView(UpdateView):
+    model = get_user_model()
+    form_class = CustomUserCreationForm
+    template_name = 'user_change.html'
+    context_object_name = 'user_obj'
+
+    def form_valid(self, form):
+        form.save()
+        update_session_auth_hash(self.request, form)
+        return super(UserChangeView, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse('user_detail', kwargs={'pk': self.object.pk})
