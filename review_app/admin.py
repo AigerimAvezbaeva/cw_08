03b487f44ff7e15fa8bb2fbc3556c795cf0330ff
Aigerim Avezbaeva <av21m1999@gmail.com>
2023-03-25 14:02:39 +0600

    добавила панель админа

--- review_app/admin.py
+++ review_app/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from review_app.models import Product, Review
+
 
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category')
+    list_filter = ('title', 'category')
+    search_fields = ('title', 'category')
+    fields = ('id', 'title', 'category', 'description', 'image')
+    readonly_fields = ('id',)
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('author', 'product', 'rating')
+    list_filter = ('author', 'product')
+    search_fields = ('author', 'product')
+    fields = ('id', 'author', 'product', 'review', 'rating')
+    readonly_fields = ('id',)
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Review, ReviewAdmin)
+
